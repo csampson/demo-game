e861297b4b278c06efddb330c836970706897361
Cameron Sampson <csampson.js@gmail.com>
2018-04-26 13:22:25 -0500

    Heartbeat implementation

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,12 @@ const { Server, OPEN: STATE_OPEN } = require('uws')
 const server = new Server({ port: 3000 })
 
 server.on('connection', socket => {
+  socket.isAlive = true
+
+  socket.on('pong', () => {
+    this.isAlive = true
+  })
+
   socket.on('message', (message) => {
     console.log(message)
 
@@ -13,6 +19,17 @@ server.on('connection', socket => {
       }
     })
   })
+
+  setInterval(() => {
+    server.clients.forEach(client => {
+      if (!client.isAlive) {
+        return socket.terminate()
+      }
+
+      socket.isAlive = false
+      socket.ping(() => {})
+    })
+  }, 1000)
 })
 
 server.broadcast = (message) => {
